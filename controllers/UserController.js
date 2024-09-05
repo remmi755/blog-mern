d51bd5515e94fba3b30d6eb8178a375b91a99db9
remmi755 <90457721+remmi755@users.noreply.github.com>
2024-09-05 10:56:07 +0300

    fix path

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -3,102 +3,108 @@ import UserModel from "../models/User.js";
 import jwt from "jsonwebtoken";
 
 export const register = async (req, res) => {
-    try {
-        const password = req.body.password;
-        const salt = await bcrypt.genSalt(10);
-        const hash = await bcrypt.hash(password, salt);
+  try {
+    const password = req.body.password;
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
 
-        const doc = new UserModel({
-            email: req.body.email,
-            fullName: req.body.fullName,
-            avatarUrl: req.body.avatarUrl,
-            passwordHash: hash,
-        })
+    const doc = new UserModel({
+      email: req.body.email,
+      fullName: req.body.fullName,
+      avatarUrl: req.body.avatarUrl,
+      passwordHash: hash,
+    });
 
-        const user = await doc.save();
+    const user = await doc.save();
 
-        const token = jwt.sign({
-                _id: user._id
-            }, 'secret123',
-            {
-                expiresIn: '90d',
-            }
-        );
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "900d",
+      }
+    );
 
-        const {passwordHash, ...userdata} = user._doc
+    const { passwordHash, ...userdata } = user._doc;
 
-        res.json({
-            ...userdata,
-            token,
-        })
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({
-            message: "Не удалось создать статью",
-        })
-    }
-}
+    res.json({
+      ...userdata,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось создать статью",
+    });
+  }
+};
 
 export const login = async (req, res) => {
-    try{
-        const user = await UserModel.findOne({email:req.body.email});
+  try {
+    const user = await UserModel.findOne({ email: req.body.email });
 
-        if(!user) {
-            return res.status(404).json({
-                message: 'Пользователь не найден'
-            })
-        }
-
-        const isValidPass = await bcrypt.compare(req.body.password, user._doc.passwordHash);
-
-        if (!isValidPass) {
-            return res.status(400).json({
-                message: 'Неверный логин или пароль'
-            })
-        }
-
-        const token = jwt.sign({
-                _id: user._id
-            }, 'secret123',
-            {
-                expiresIn: '30d',
-            }
-        );
-
-        const {passwordHash, ...userdata} = user._doc
-
-        res.json({
-            ...userdata,
-            token,
-        })
-
-    } catch (err) {
-        console.log(err)
-        res.status(500).json({
-            message: "Не удалось авторизоваться",
-        })
+    if (!user) {
+      return res.status(404).json({
+        message: "Пользователь не найден",
+      });
     }
-}
+
+    const isValidPass = await bcrypt.compare(
+      req.body.password,
+      user._doc.passwordHash
+    );
+
+    if (!isValidPass) {
+      return res.status(400).json({
+        message: "Неверный логин или пароль",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        _id: user._id,
+      },
+      "secret123",
+      {
+        expiresIn: "30d",
+      }
+    );
+
+    const { passwordHash, ...userdata } = user._doc;
+
+    res.json({
+      ...userdata,
+      token,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({
+      message: "Не удалось авторизоваться",
+    });
+  }
+};
 
 export const getMe = async (req, res) => {
-    try {
-        const user = await UserModel.findById(req.userId)
-        
-        if(!user) {
-            return res.status(404).json({
-                message: 'Пользователь не найден'
-            })
-        }
+  try {
+    const user = await UserModel.findById(req.userId);
 
-        const {passwordHash, ...userdata} = user._doc
-
-        res.json({
-            ...userdata,
-        })
-    } catch (err) {
-        console.log(err)
-        return res.status(500).json({
-            message: 'Нет доступа'
-        })
+    if (!user) {
+      return res.status(404).json({
+        message: "Пользователь не найден",
+      });
     }
-}
+
+    const { passwordHash, ...userdata } = user._doc;
+
+    res.json({
+      ...userdata,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Нет доступа",
+    });
+  }
+};
--- index.js
+++ index.js
@@ -1,67 +1,96 @@
-import express from 'express';
-import fs from 'fs'
+import express from "express";
+import fs from "fs";
 import mongoose from "mongoose";
-import multer from 'multer'
-import cors from 'cors'
+import multer from "multer";
+import cors from "cors";
 
-import { registerValidation, loginValidation, postCreateValidation } from "./validations.js";
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from "./validations.js";
 
-import { handleValidationErrors, checkAuth }  from './utils/index.js'
+import { handleValidationErrors, checkAuth } from "./utils/index.js";
 
 import { UserController, PostController } from "./controllers/index.js";
 
-mongoose.connect('mongodb+srv://remmi:wwwwww@cluster0.j5xu8.mongodb.net/blog?retryWrites=true&w=majority')
-    .then(() => {
-        console.log("DB  OK")
-    })
-    .catch((error) => console.log('DB error', error))
+mongoose
+  .connect(
+    "mongodb+srv://remmi:wwwwww@cluster0.j5xu8.mongodb.net/blog?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("DB  OK");
+  })
+  .catch((error) => console.log("DB error", error));
 
 const app = express();
 
 const storage = multer.diskStorage({
-    destination:(_,__, cb) => {
-        if(!fs.existsSync('uploads')) {
-            fs.mkdirSync('uploads')
-        }
-        cb(null, 'uploads')
-    },
-    filename:(_, file, cb) => {
-        cb(null, file.originalname)
-    },
-})
+  destination: (_, __, cb) => {
+    if (!fs.existsSync("uploads")) {
+      fs.mkdirSync("uploads");
+    }
+    cb(null, "uploads");
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
 
-const upload = multer({storage})
+const upload = multer({ storage });
 
 app.use(express.json());
-app.use(cors())
-app.use('/uploads', express.static('uploads'))
-app.use('/uploads/avatar', express.static('uploads'))
+app.use(cors());
+app.use("/uploads", express.static("uploads"));
+app.use("/uploads/avatar", express.static("uploads"));
 
-app.post('/auth/login',loginValidation, handleValidationErrors, UserController.login)
-app.post('/auth/register', registerValidation, handleValidationErrors,  UserController.register)
-app.get('/auth/me', checkAuth, UserController.getMe );
+app.post(
+  "/auth/login",
+  loginValidation,
+  handleValidationErrors,
+  UserController.login
+);
+app.post(
+  "/auth/register",
+  registerValidation,
+  handleValidationErrors,
+  UserController.register
+);
+app.get("/auth/me", checkAuth, UserController.getMe);
 
-app.post('/upload',checkAuth, upload.single('image'), (req, res) => {
-    res.json({
-        url: `/uploads/${req.file.originalname}`
-    })
-})
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
+  res.json({
+    url: `/uploads/${req.file.originalname}`,
+  });
+});
 
-app.get('/tags', PostController.getLastTags );
-app.get('/comments', PostController.getLastComments );
-app.get('/posts', PostController.getAll );
-app.get('/tags/:name', PostController.sortByTag );
-app.patch('/posts', PostController.sortByNewest );
-app.get('/posts/tags', PostController.getLastTags );
-app.get('/posts/:id', PostController.getOne );
-app.post('/comments/:id', checkAuth, PostController.createComment);
-app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create );
-app.patch('/posts/:id', checkAuth,postCreateValidation, handleValidationErrors,PostController.update );
-app.delete('/posts/:id', checkAuth, PostController.remove );
+app.get("/tags", PostController.getLastTags);
+app.get("/comments", PostController.getLastComments);
+app.get("/posts", PostController.getAll);
+app.get("/tags/:name", PostController.sortByTag);
+app.patch("/posts", PostController.sortByNewest);
+app.get("/posts/tags", PostController.getLastTags);
+app.get("/posts/:id", PostController.getOne);
+app.post("/comments/:id", checkAuth, PostController.createComment);
+app.post(
+  "/posts",
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.create
+);
+app.patch(
+  "/posts/:id",
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.update
+);
+app.delete("/posts/:id", checkAuth, PostController.remove);
 
-app.listen( process.env.PORT || 4444, (err) => {
-    if (err) {
-        return console.log(err)
-    }
-    console.log("Server Ok")
-})
+app.listen(process.env.PORT || 4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log("Server Ok");
+});
